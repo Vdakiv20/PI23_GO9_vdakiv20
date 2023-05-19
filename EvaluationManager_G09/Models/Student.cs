b9ef2640f015b307cc21e7f6f451c14320717a7d
Vdakiv20 <100692453+Vdakiv20@users.noreply.github.com>
2023-05-19 20:00:48 +0200

    Implementacija hijerarhije

---
+++ EvaluationManager_G09/Models/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvaluationManager_G09.Models {
+    public class Student {
+        public int Grade { get; set; }
+    }
+}

d5521e47ba7b7ba03d427b51d403fafbfc1f3dd6
Vdakiv20 <100692453+Vdakiv20@users.noreply.github.com>
2023-05-22 21:47:20 +0200

    Dodana klasa Evaluation

--- EvaluationManager_G09/Models/Teacher.cs
+++ EvaluationManager_G09/Models/Teacher.cs
@@ -8,5 +8,10 @@ namespace EvaluationManager_G09.Models {
     public class Teacher : Person {
         public string Username { get; set; }
         public string Password { get; set; }
+
+        public bool CheckPassword(string password) {
+            return Password == password;
+        }
+
     }
 }
